daf288a02d9ac3f9335b1e58f766ff3617201e7a
Andrew Or <andrewor14@gmail.com>
2020-11-18 17:12:25 -0500

    Add script to produce randomized grouping

---
+++ coffee.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import random
+
+participants = [
+  "Andrew",
+  "Amit",
+  "Sam",
+  "Theano",
+  "Jianan",
+  "Jeff",
+  "Anja",
+  "Natalie",
+  #"Khiem",
+  "Zhenyu",
+  "Ashwini",
+  "David",
+  #"Dongsheng",
+  "Nan",
+  #"Wyatt",
+  "Yue",
+  "Chris",
+  "Jennifer",
+  "Yushan"
+]
+
+group_size = 3
+num_groups = len(participants) // group_size
+random.shuffle(participants)
+grouping = np.array_split(participants, num_groups)
+for i, g in enumerate(grouping):
+  print("Group %s: %s" % (i, ", ".join(g)))
+
